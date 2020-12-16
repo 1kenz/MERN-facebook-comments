06fc0c4c647db1812e7806465ba899205bf427b4
xkendx <63884610+xkendx@users.noreply.github.com>
2020-12-16 14:09:54 +0300

    fix: fix db connect

--- models/commentsDB.js
+++ models/commentsDB.js
@@ -1,24 +1,22 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+
 const CommentSchema = new mongoose.Schema({
+  author: {
+    type: String,
+    required: true,
+  },
+  comment: {
+    type: String,
+    required: true,
+  },
+  registeredAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-    author:
-    {
-        type:String,
-        required:true
-    },
-    comment:
-    {
-        type:String,
-        required:true
-    },
-    registeredAt: {
-        type: Date,
-        default: Date.now,
-      },
-    updatedAt:{
-        type: Date,
-        default: Date.now,
-      },
-})
-
-module.exports = Info = mongoose.model('comment', CommentSchema)
+module.exports = Info = mongoose.model("comment", CommentSchema);
