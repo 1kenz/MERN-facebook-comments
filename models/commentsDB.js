a496ff73c699780eb3005aca4ade0e4452dcfc81
xkendx <63884610+xkendx@users.noreply.github.com>
2020-12-19 02:56:49 +0300

    feat: update frontend

--- models/commentsDB.js
+++ models/commentsDB.js
@@ -1,22 +1,24 @@
-const mongoose = require("mongoose");
-
+const mongoose = require('mongoose');
 const CommentSchema = new mongoose.Schema({
-  author: {
-    type: String,
-    required: true,
-  },
-  comment: {
-    type: String,
-    required: true,
-  },
-  registeredAt: {
-    type: Date,
-    default: Date.now,
-  },
-  updatedAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
 
-module.exports = Info = mongoose.model("comment", CommentSchema);
+    author:
+    {
+        type:String,
+        required:true
+    },
+    comment:
+    {
+        type:String,
+        required:true
+    },
+    registeredAt: {
+        type: Date,
+        default: Date.now,
+      },
+    updatedAt:{
+        type: Date,
+        default: Date.now,
+      },
+})
+
+module.exports = Info = mongoose.model('comment', CommentSchema)
--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require("express");
 const app = express();
 // const router = express.Router();
 const connectDB = require("./models/connectDB");
-const router = require("./routes/router");
+const router = require("./routers/router");
+<<<<<<< HEAD
+// const cors = require("cors");
+=======
+const cors = require('cors');
+>>>>>>> 83b09e257728d9acacb1ce4d7336018384c8fd65
 
 require("dotenv").config();
-
-connectDB();
-
-app.use(express.json());
-app.use("/", router);
 
 // production
 if (process.env.NODE_ENV === "production") {
@@ -23,3 +23,15 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`I'm listening on port ${port}`);
 });
+<<<<<<< HEAD
+// app.use(cors());
+=======
+
+app.use(cors());
+>>>>>>> 83b09e257728d9acacb1ce4d7336018384c8fd65
+app.use(express.json());
+app.use("/", router);
+
+connectDB();
+
+// victor deneme
